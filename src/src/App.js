f850a7c208d09e87d51da5b9529c02afffb3f19c
motasemAlsqoor <motasem900khaled@gmail.com>
2021-05-04 09:27:45 +0300

    done

--- src/src/App.js
+++ src/src/App.js
@@ -2,16 +2,30 @@
 import Header from './components/header/Header.js';
 import Footer from './components/footer/Footer.js';
 import Form from './components/form/Form.js';
+import Result from './components/result/Result.js'
 import './App.scss';
-
-function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <Form/>
-      <Footer/>
-    </div>
-  );
+import { Component } from 'react';
+class App extends Component{
+  constructor(probs){
+    super(probs);
+    this.state={
+      result:[]
+    }
+  }
+  handler=(result)=>{
+    this.setState({result})
+  }
+  render(){
+    return (
+      <div className="App">
+        <Header/>
+        <Form prompt="get result" handler={this.handler}/>
+        <Result result={this.state.result}/>
+        <Footer/>
+      </div>
+    );
+  }
 }
+
 
 export default App;
--- src/src/components/form/Form.js
+++ src/src/components/form/Form.js
@@ -1,49 +1,50 @@
 import React from 'react'
 import './Form.scss'
 class Form extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      formDate:{
-        method:null,
-        url:null
-      }
+    this.state = {
+      handler: props.handler
     }
+
   }
-  formHandler=(event)=>{
+  formHandler = (event) => {
     event.preventDefault();
-    const url= event.target.url.value;
-    const method=event.target.method.value;   
-    const formDate={
-      url:url,
-      method:method
+    const url = event.target.url.value;
+    const method = event.target.method.value;
+
+    this.getData(url)
+
+  }
+  getData = async (url) => {
+    try {
+      const raw = await fetch(url);
+      const result = await raw.json();
+      this.state.handler(result.results);
+    } catch (error) {
+      //console.log(error);
     }
-    this.setState({formDate});
   }
 
-  render(){
+  render() {
     return (
       <>
         <form onSubmit={this.formHandler}>
-            <label htmlFor="url">URL</label>
-            <input type="text" name="url"/>
-            <input type="submit" value="Go" />
-            <br/>
-            <label htmlFor="method">Get</label>
-            <input type="radio" name="method" value="get"/>
-            <label htmlFor="method">Post</label>
-            <input type="radio" name="method" value="post"/>
-            <label htmlFor="method">Put</label>
-            <input type="radio" name="method" value="put"/>
-            <label htmlFor="method">delete</label>
-            <input type="radio" name="method" value="delete"/>
+          <label htmlFor="url">URL</label>
+          <input type="text" name="url" />
+          <input type="submit" value="Go" />
+          <br />
+          <label htmlFor="method">Get</label>
+          <input type="radio" name="method" value="get" />
+          <label htmlFor="method">Post</label>
+          <input type="radio" name="method" value="post" />
+          <label htmlFor="method">Put</label>
+          <input type="radio" name="method" value="put" />
+          <label htmlFor="method">delete</label>
+          <input type="radio" name="method" value="delete" />
         </form>
-        <div id="form-data">
-          <p>{this.state.formDate.method}</p>
-          <p>{this.state.formDate.url}</p>
-        </div>
-        </>
+      </>
     );
-  } 
+  }
 }
 export default Form;
